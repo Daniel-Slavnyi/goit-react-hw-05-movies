cbd59ae7ecd17f6d1067754ed561b69b2966fb3e
Daniel-Slavnyi <113385648+Daniel-Slavnyi@users.noreply.github.com>
2023-01-14 11:36:17 +0000

    Deploying to gh-pages from @ Daniel-Slavnyi/goit-react-hw-05-movies@7943f0077c5f987c7d85a25735b19c62dd43b3bf 🚀

--- static/js/983.0497be1e.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[983],{983:function(e,n,t){t.r(n),t.d(n,{default:function(){return c}});t(791);var r=t(87),o=t(184);function c(){return(0,o.jsxs)(o.Fragment,{children:[(0,o.jsx)("p",{children:"NotPageFound404"}),(0,o.jsx)(r.rU,{to:"/",children:"Go Home"})]})}}}]);
-//# sourceMappingURL=983.0497be1e.chunk.js.map
---
+++ static/js/983.47071171.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[983],{7983:function(e,n,t){t.r(n),t.d(n,{default:function(){return c}});t(2791);var r=t(1087),o=t(184);function c(){return(0,o.jsxs)(o.Fragment,{children:[(0,o.jsx)("p",{children:"NotPageFound404"}),(0,o.jsx)(r.rU,{to:"/",children:"Go Home"})]})}}}]);
+//# sourceMappingURL=983.47071171.chunk.js.map
